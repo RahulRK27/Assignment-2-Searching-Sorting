06aaac63290b485ccec26be718cf5a172d0c0b46
rahul khapre <121964920+RahulRK27@users.noreply.github.com>
2023-03-21 16:11:37 +0530

    Add files via upload
    Assignment-2: Searching & Sorting

---
+++ Assignment2_que5.py
@@ -0,0 +1,10 @@
+## Q:5
+def sort_strings(strings):
+    return sorted(strings)
+
+strings = ["banana", "apple", "cherry", "date", "fig"]
+sorted_strings = sort_strings(strings)
+print(sorted_strings)
+strings = ["orange", "banana", "apple", "watermelon", "cherry", "grape", "date", "fig", "kiwi"]
+sorted_strings = sort_strings(strings)
+print(sorted_strings)
